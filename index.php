584eb1560382de842871b43393374827aa814d89
abhiambre14 <abhiambre4@gmail.com>
2025-01-12 18:35:43 +0530

    final changes

---
+++ index.php
@@ -0,0 +1,439 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>THERALUX™</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link rel="stylesheet" href="https://use.typekit.net/jto0jrt.css">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="index-page">
+
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container position-relative row align-items-center mx-auto">
+
+      <a href="index.html" class="logo col-6 text-centers align-items-center mx-auto">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <img src="assets/img/logo/logo.svg" alt="">
+      </a>
+
+      <nav id="navmenu" class="col-2 navmenu">
+        <ul>
+          <li><a href="#hero" class="active">Home<br></a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#team">Team</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="#">Dropdown 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i
+                    class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">Deep Dropdown 1</a></li>
+                  <li><a href="#">Deep Dropdown 2</a></li>
+                  <li><a href="#">Deep Dropdown 3</a></li>
+                  <li><a href="#">Deep Dropdown 4</a></li>
+                  <li><a href="#">Deep Dropdown 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Dropdown 2</a></li>
+              <li><a href="#">Dropdown 3</a></li>
+              <li><a href="#">Dropdown 4</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <i class="mobile-nav-toggle bi bi-list"></i>
+      </nav>
+
+      <a class="btn-getstarted col-2 text-center flex-md-shrink-0" href="#contact">CONTACT US</a>
+
+    </div>
+  </header>
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section">
+
+      <div class="container">
+        <div class="d-flex h-100 gy-4">
+          <div class="col-md-12 mx-auto order-2 text-center order-lg-1 d-flex flex-column justify-content-center">
+            <h1 class="text-uppercase" data-aos="fade-up">Crafting a legacy of wellness for India’s elite!</h1>
+          </div>
+        </div>
+      </div>
+
+    </section><!-- /Hero Section -->
+    <!-- Values Section -->
+    <section id="values" class="values section">
+
+      <!-- Section Title -->
+
+
+      <div class="container">
+
+        <div class="row gy-4">
+          <div class="col-lg-12 col-md-12 col-sm-12">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item">Excellence</li>
+                <li class="breadcrumb-item">Integrity</li>
+                <li class="breadcrumb-item">Innovation</li>
+              </ol>
+            </nav>
+          </div>
+          <div class="col-lg-12 m-0">
+            <div class="section-title text-left" data-aos="fade-up">
+              <h2 class="text-uppercase">A bespoke journey of health,tailored to every family’s unique needs.</h2>
+            </div><!-- End Section Title -->
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <img src="assets/img/asset/2.jpg" alt="" class="img-fluid">
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-6 pl-3" data-aos="fade-up" data-aos-delay="200">
+            <p class="ft-16 lh-28 text-justify  ">In today’s fast-paced world, Indian families often juggle multiple
+              roles, priorities, and
+              expectations,
+              leaving little time to focus on overall health and well-being. At THERALUX™, we understand the unique
+              challenges faced by Indian households, and we are here to help you prioritize what matters most:
+              <strong class="text-uppercase">your holistic wellness.</strong>
+            </p>
+          </div><!-- End Card Item -->
+
+
+        </div>
+
+      </div>
+
+    </section><!-- /Values Section -->
+
+    <!-- Stats Section -->
+    <section id="stats" class="pt-0 mt-60 stats section position-relative overflow-hidden">
+
+      <div class="">
+        <div class="row gy-4 align-items-start " data-aos="fade-up" data-aos-delay="100">
+          <div class="col">
+            <hr class="color-blue " />
+          </div>
+          <div class="width-max-content">
+            <h5 class="text-uppercase text-justify ft-24">Nobility and Novelty</h5>
+          </div><!-- End Stats Item -->
+
+          <div class="col-3 p-0">
+            <hr class="color-blue " />
+          </div><!-- End Stats Item -->
+
+          <div class="width-max-content">
+            <h5 class="text-uppercase text-wrap mx-auto text-justify ft-24">a PLACE where timeless values meet <br>
+              innovative wellness, all rooted in <br> integrity and excellence.</h5>
+          </div><!-- End Stats Item -->
+          <div class="col">
+            <hr class="color-blue " />
+          </div>
+        </div>
+      </div>
+
+    </section><!-- /Stats Section -->
+
+
+    <section id="about" class="about section">
+
+      <div class="container">
+        <div class="d-grid grid-2-1 gap-100 align-items-center" data-aos="fade-up" data-aos-delay="100">
+
+          <div class="p-0">
+            <img src="assets/img/asset/chart.svg" alt="" class="img-fluid w-100">
+          </div><!-- End Stats Item -->
+
+          <div class="p-0">
+            <p class="text-left ft-16 regular text-uppercase m-0">Isolation is not the solution,<br> THERALUX™ offers
+              India’s only <br> comprehensive wellness solutions. <br> –</p>
+            <img src="assets/img/asset/3.jpg" alt="" class="img-fluid w-100">
+            <p class="text-left ft-16 regular text-uppercase m-0"> – <br> We define holistic wellness through <br> five
+              axiomatic pillars.</p>
+          </div><!-- End Stats Item -->
+
+        </div>
+      </div>
+
+    </section><!-- /Stats Section -->
+    <section id="feature" class="feature section">
+
+      <div class="container">
+        <div class="d-grid grid-1-1 gap-100" >
+
+          <div class="detail-box d-grid gap-50">
+            <div class="d-grid grid-1-2 gap-10">
+              <div class="p-0 m-0" data-aos="fade-right" data-aos-delay="300">
+                <img src="assets/img/asset/arrow1.svg" alt="" class="img-fluid">
+              </div>
+              <div class="p-0 m-0 " data-aos="fade-right" data-aos-delay="200">
+                <p class="text-justify m-0">With the THERALUX™ Wellness Team led by health specialists, we create a
+                  carefully curated experience that aligns with your family’s values and needs. Through continuous
+                  expert-guided consultations and assessments conducted by your THERALUX™ Wellness Manager, we offer
+                  holistic solutions that are tailor-fit.</p>
+              </div>
+            </div>
+            <div class="p-0 m-0" data-aos="fade-right" data-aos-delay="100">
+              <img src="assets/img/asset/4.jpg" alt="" class="img-fluid object-cover object-center aspect-ratio-4-3 w-100">
+            </div>
+          </div><!-- End Stats Item -->
+
+          <div class="detail-box d-grid gap-100">
+            <div class="p-0 m-0" data-aos="fade-left" data-aos-delay="100">
+              <img src="assets/img/asset/5.jpg" alt="" class="aspect-ratio-4-3 object-cover object-center img-fluid w-100">
+            </div>
+            <div class="d-grid grid-2-1 gap-10 align-items-end">
+              <div class="p-0 m-0 " data-aos="fade-left" data-aos-delay="200">
+                <p class="text-justify m-0">We’re not just a brand — we’re a partner in your family’s journey to
+                  long-lasting health. Let THERALUX™ bring balance, vitality, and joy back into your life. Because your
+                  family deserves to thrive, not just survive.</p>
+              </div>
+              <div class="p-0 m-0" data-aos="fade-left" data-aos-delay="300">
+                <img src="assets/img/asset/arrow2.svg" alt="" class="img-fluid">
+              </div>
+            </div>
+
+          </div><!-- End Stats Item -->
+
+        </div>
+      </div>
+
+    </section><!-- /Stats Section -->
+    <section id="separator" class="separator section">
+      <div class="d-flex align-items-center">
+        <div class="width-auto col">
+          <hr class="text-red border-width-2" />
+        </div>
+        <div class="width-max-content px-20">
+          <h4 class="text-red m-0 text-uppercase">Wellness Rooted in Tradition, Empowered by Modern Science</h4>
+        </div>
+        <div class="width-auto col">
+          <hr class="text-red border-width-2" />
+        </div>
+      </div>
+    </section>
+
+    <!-- Team Section -->
+    <section id="team" class="team section">
+      <div class="container">
+        <div class="row gap-30">
+          <div class="col align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+            <div class="team-member">
+              <div class="member-img">
+                <img src="assets/img/team/pratham.svg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-left">
+                <h4>Pratham Aggarwal</h4>
+                <span>Co-Founder | CEO</span>
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+          <div class="col align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+            <div class="team-member text-left">
+              <div class="member-img">
+                <img src="assets/img/team/1.svg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Riya Aggarwal</h4>
+                <span>Co-Founder | CEO</span>
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+          <div class="col align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+            <div class="team-member">
+              <div class="member-img ">
+                <img src="assets/img/team/parth.svg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-left">
+                <h4>Parth Aggarwal</h4>
+                <span>Co-Founder | CEO</span>
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+
+        </div>
+
+      </div>
+
+    </section><!-- /Team Section -->
+
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact section py-120">
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+        <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
+          <div class="row gap-30">
+            <div class="col align-items-stretch info-item" data-aos="fade-up" data-aos-delay="300"">
+              <p>Would you like to know more <br>
+                about how we can support your <br>
+                family’s health and happiness?</p>
+            </div>
+            <div class=" col"></div>
+            <div class="col"></div>
+          </div>
+          <div class="row gap-30 align-items-end">
+
+            <div class="col">
+              <div class="row ">
+                <div class="width-max-content info-item">
+                  <h5 class="text-red m-0">CONTACT US</h5>
+                </div>
+                <div class="col">
+                  <img src="assets/img/asset/arrow3.svg" alt="" class="img-fluid">
+                </div>
+              </div>
+            </div>
+            <div class="col-8">
+              <div class="gy-4">
+                <div class="row">
+                  <div class="col">
+                    <div class="form-floating">
+                      <input type="text" name="fname" class="form-control" placeholder="" required>
+                      <label for="fname">FIRST NAME</label>
+                    </div>
+                  </div>
+
+                  <div class="col">
+                    <div class="form-floating">
+                      <input type="text" name="lname" class="form-control" placeholder="" required>
+                      <label for="lname">LAST NAME</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div><!-- End Contact Form -->
+
+          </div>
+          <div class="row gap-30 align-items-end">
+            <div class="col">
+              <div class="col-md-12 mt-60">
+                <button class="form-btn" type="submit">SUBMIT</button>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="row mt-60">
+                <div class="col">
+                  <div class="form-floating">
+                    <input type="email" name="email" class="form-control" placeholder="" id="email" required>
+                    <label for="email">E-MAIL Id</label>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="row gap-10 m-0 p-0">
+                    <div class="col p-0">
+                      <div class="form-floating">
+                        <input type="text" name="gender" class="form-control" id="gender" placeholder="" required>
+                        <label for="gender">GENDER</label>
+                      </div>
+                    </div>
+                    <div class="col p-0">
+                      <div class="form-floating">
+                        <input type="text" name="age" class="form-control" id="age" placeholder="" required>
+                        <label for="age">AGE</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 text-center">
+            <div class="loading">Loading</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Your message has been sent. Thank you!</div>
+
+
+          </div>
+        </form>
+
+      </div>
+
+    </section><!-- /Contact Section -->
+
+  </main>
+
+  <footer id="footer" class="footer ">
+    <div class="container ">
+      <div class="row gap-30 m-0 align-items-center">
+        <div class="col m-0 p-0">
+          <a href="index.html" class="d-flex align-items-center">
+            <img src="assets/img/logo/footer logo.svg" class="img-fluid" alt="">
+          </a>
+        </div>
+        <div class="col"></div>
+        <div class="col m-0 p-0">
+          <div class="d-flex m-0 justify-content-evenly gap-10 align-items-center">
+            <div class="footer-icons">
+              <a href="https://www.instagram.com/theralux.wellness" target="_blank" rel="noopener noreferrer"><img
+                  src="assets/img/service-icon/instagram.svg" alt="" class="img-fluid"></a>
+            </div>
+            <div class="footer-icons">
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <img src="assets/img/service-icon/facebook.svg" alt="" class="img-fluid">
+              </a>
+            </div>
+            <div class="footer-icons">
+              <a href="https://x.com/theralux_in" target="_blank" rel="noopener noreferrer">
+                <img src="assets/img/service-icon/twitter.svg" alt="" class="img-fluid">
+              </a>
+            </div>
+            <div class="footer-icons">
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <img src="assets/img/service-icon/whatsapp.svg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </footer>
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
